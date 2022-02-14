cd8f38f4e8efdc9f2d610d33ed6cde2f9983e830
Simon <simonbesteiro@gmail.com>
2022-02-14 12:45:45 +0100

    green

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,28 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TripServiceTest {
-	
+
+    private User user;
+
+    @Test void
+    return_exception_given_null_user(){
+        TripService tripService = new testeableTripService();
+        user = null;
+        assertThrows(UserNotLoggedInException.class , ()->{
+            tripService.getTripsByUser(user);
+        });
+
+    }
+
+    private class testeableTripService extends TripService {
+        protected User getLoggedUser(){
+            return  null;
+        }
+    }
 }
